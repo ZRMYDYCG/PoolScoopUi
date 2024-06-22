31fc8e89d353567140ffbc915a0ac7b7b228fa84
ZRMYDYCG <547471919@qq.com>
2024-06-22 12:16:08 +0800

    为.vue文件田间类型提示

--- node_modules/play/src/vite-env.d.ts
+++ node_modules/play/src/vite-env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+
+/**
+ * 为 .vue 文件做类型提示
+ * */ 
+declare module '*.vue' {
+    import type { DefineComponent } from "vue"
+    const component: DefineComponent<{}, {}, any>
+    export default component
+}
